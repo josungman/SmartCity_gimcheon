ddf41ca80f279dfae04dcda996f3a2fc078324d6
josungman <cgu999@naver.com>
2020-09-09 01:29:20 +0900

    20.09.09 GIS EPSG CHANGE

---
+++ New_DB_Project(GIS)/src/COM/GisCore.java
@@ -0,0 +1,197 @@
+package COM;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.indigo.cep.epl.generated.CepEPL2GrammarParser.arrayExpression_return;
+
+public class GisCore extends QuartzJobBean {
+
+	private Log log = LogFactory.getLog(GisCore.class);
+	
+	private String ttspath;
+	
+	private dao Dao = new dao();
+
+	private String beforeEPSG;
+	
+	private String afterEPSG;
+	
+	private String targetTable;
+	
+	@Override
+	protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
+		// TODO Auto-generated method stub
+
+		try {
+			onSignal();
+		}
+
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	List<HashMap<String, Object>> rowList = null;
+
+	// job(onSignal) 메소드
+	@SuppressWarnings("null")
+	private void onSignal() throws Exception {
+
+		
+		long start = System.currentTimeMillis();
+
+		
+		
+		// Select 쿼리에 들어갈 HashMap 타입
+		HashMap<String, Object> targetmap = new HashMap<String, Object>();
+		
+		// DB Select
+		targetmap.put("targetTable", targetTable);
+		rowList = Dao.select("SQL",targetmap);
+
+		String[] LON = null;
+		String[] LAT = null;
+		String[] FCLT_ID = null;
+
+		
+		
+		// Select된 데이터 Jason 변환
+		if (!rowList.isEmpty()) {
+
+			JSONArray array = new JSONArray();
+			for (HashMap<String, Object> map : rowList) {
+				array.put(new JSONObject(map));
+			}
+			String msg = array.toString();
+
+			log.info("JSON : " + msg);
+
+			
+			
+			// Jason을 다시 분리하여 각각의 배열변수에 다시 대입
+			List<HashMap<String, Object>> AddList = new ArrayList<HashMap<String, Object>>();
+
+			LON = new String[array.length()];
+			LAT = new String[array.length()];
+			FCLT_ID = new String[array.length()];
+
+			for (int i = 0; i < array.length(); i++) {
+				
+				
+				// jsonStringToMap메서드 이용하여 맵 형식으로 변환
+				AddList.add(jsonStringToMap(array.getJSONObject(i)));
+
+				
+				
+				// 배열 변수에 대입
+				LON[i] = AddList.get(i).get("LON").toString();
+				LAT[i] = AddList.get(i).get("LAT").toString();
+				FCLT_ID[i] = AddList.get(i).get("FCLT_ID").toString();
+
+			}
+
+			
+			
+			// 좌표변환 후 쿼리 업데이트
+			for (int i = 0; i < array.length(); i++) {
+
+				double X = 0;
+				double Y = 0;
+
+				// 좌표변환 메서드X
+				X = Dao.Change_GIS_X(beforeEPSG,afterEPSG, LON[i], LAT[i]);
+
+				// 좌표변환 메서드Y
+				Y = Dao.Change_GIS_Y(beforeEPSG,afterEPSG, LON[i], LAT[i]);
+
+				// Update 쿼리에 들어갈 HashMap 타입
+				HashMap<String, Object> Datamap = new HashMap<String, Object>();
+
+				Datamap.put("FCLT_ID", FCLT_ID[i]);
+				Datamap.put("LAT", X);
+				Datamap.put("LON", Y);
+				
+				//EPSG: 는 제외
+				Datamap.put("afterEPSG", afterEPSG.substring(5));
+				Datamap.put("targetTable", targetTable);
+			
+				Dao.update("SQL", Datamap);
+
+			}
+			long end = System.currentTimeMillis();
+
+			log.info("");
+			log.info("===================================================");
+			log.info("================= GIS JOb Finish ==================");
+			log.info("========= 동작시간 : " + ( end - start )/1000.0 + " =========");
+			log.info("");
+		}
+
+	}
+
+	
+	public HashMap<String, Object> jsonStringToMap(JSONObject jsonMsg) throws IOException {
+		return new ObjectMapper().readValue(jsonMsg.toString(), new TypeReference<Map<String, Object>>() {
+		});
+	}
+
+	
+	
+	
+	
+	
+	
+//	=============================================
+	
+	
+	
+	
+	
+	
+	
+	public String getBeforeEPSG() {
+		return beforeEPSG;
+	}
+
+
+	public void setBeforeEPSG(String beforeEPSG) {
+		this.beforeEPSG = beforeEPSG;
+	}
+
+
+	public String getAfterEPSG() {
+		return afterEPSG;
+	}
+
+
+	public void setAfterEPSG(String afterEPSG) {
+		this.afterEPSG = afterEPSG;
+	}
+
+
+	public String getTargetTable() {
+		return targetTable;
+	}
+
+
+	public void setTargetTable(String targetTable) {
+		this.targetTable = targetTable;
+	}
+
+}
---
+++ New_DB_Project(GIS)/src/COM/dao.java
@@ -0,0 +1,153 @@
+package COM;
+
+import java.util.HashMap;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.codehaus.jettison.json.JSONObject;
+import org.osgeo.proj4j.BasicCoordinateTransform;
+import org.osgeo.proj4j.CRSFactory;
+import org.osgeo.proj4j.CoordinateReferenceSystem;
+import org.osgeo.proj4j.ProjCoordinate;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.apache.ibatis.session.SqlSession;
+
+public class dao {
+
+	private Log log = LogFactory.getLog(dao.class);
+
+	static SqlSessionFactory sqlSessionFactory;
+
+	private StringBuffer stringBuffer = new StringBuffer();
+
+	final String MAPPER_NAME = "Query";
+
+	static {
+		// DB 접속 정보
+		try {
+			String resource = "mybatis-config.xml";
+			InputStream reader = Resources.getResourceAsStream(resource);
+			if (sqlSessionFactory == null) {
+				sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<HashMap<String, Object>> select(String methodName, HashMap<String, Object> targetTable)
+			throws Exception {
+		SqlSession session = sqlSessionFactory.openSession();
+		stringBuffer.append(MAPPER_NAME).append(".select").append(methodName);
+
+		log.info("====== stringBuffer check ======");
+		log.info(stringBuffer.toString());
+		log.info("====== stringBuffer check ======");
+
+		try {
+
+			log.info("DB SELECT RESULT : " + session.selectList(stringBuffer.toString(), targetTable));
+			return session.selectList(stringBuffer.toString(), targetTable);
+		} finally {
+			session.close();
+			stringBuffer.setLength(0);
+		}
+	}
+
+	public void update(String methodName, HashMap<String, Object> paramMap) throws Exception {
+		SqlSession session = sqlSessionFactory.openSession();
+		stringBuffer.append(MAPPER_NAME).append(".update").append(methodName);
+		try {
+			log.info(" ===== update result start =====");
+
+			session.update(stringBuffer.toString(), paramMap);
+			session.commit();
+
+			log.info(" ===== update result end =====");
+
+		} finally {
+			session.close();
+			stringBuffer.setLength(0);
+		}
+	}
+
+	public Double Change_GIS_X(String beforeEPSG, String afterEPSG, String LON, String LAT) {
+		// FD_CP_CTV LAT
+		double point_x = Double.parseDouble(LAT);
+		double point_y = Double.parseDouble(LON);
+
+		CRSFactory factory = new CRSFactory();
+		CoordinateReferenceSystem srcCrs = factory.createFromName(beforeEPSG);// 현재 좌표
+		CoordinateReferenceSystem dstCrs = factory.createFromName(afterEPSG);// 변경할 좌표
+		BasicCoordinateTransform transform = new BasicCoordinateTransform(srcCrs, dstCrs);
+
+		log.info(srcCrs + " => " + dstCrs);
+
+		double x = (point_x); // x좌표(경도 LAT)
+		double y = (point_y);// y좌표(위도 LON)
+
+		ProjCoordinate srcCoord = new ProjCoordinate(y, x); // 위,경도
+		ProjCoordinate dstCoord = new ProjCoordinate();
+
+		transform.transform(srcCoord, dstCoord);// 좌표변환
+
+		// 변환된 좌표
+		log.info("Complete(_X)" + srcCoord.x + " => " + dstCoord.x);
+
+		return dstCoord.x;
+
+	}
+
+	public Double Change_GIS_Y(String beforeEPSG, String afterEPSG, String LON, String LAT) {
+		// FD_CP_CTV LAT
+		double point_x = Double.parseDouble(LAT);
+		double point_y = Double.parseDouble(LON);
+
+		CRSFactory factory = new CRSFactory();
+		CoordinateReferenceSystem srcCrs = factory.createFromName(beforeEPSG);// 현재 좌표
+		CoordinateReferenceSystem dstCrs = factory.createFromName(afterEPSG);// 변경할 좌표
+		BasicCoordinateTransform transform = new BasicCoordinateTransform(srcCrs, dstCrs);
+
+		log.info(srcCrs + " => " + dstCrs);
+
+		double x = (point_x); // x좌표(경도 LAT)
+		double y = (point_y);// y좌표(위도 LON)
+
+		ProjCoordinate srcCoord = new ProjCoordinate(y, x); // 위,경도
+		ProjCoordinate dstCoord = new ProjCoordinate();
+
+		transform.transform(srcCoord, dstCoord);// 좌표변환
+
+		// 변환된 좌표
+		log.info("Complete(_Y)" + srcCoord.y + " => " + dstCoord.y);
+
+		return dstCoord.y;
+
+	}
+
+}
