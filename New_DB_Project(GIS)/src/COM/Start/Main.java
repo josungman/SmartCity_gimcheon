d9c944d631fb42677a3b782b8e02afb7e9bdfcdb
josungman <cgu999@naver.com>
2020-09-09 21:09:37 +0900

    200909 GIS change adapter uploaded

---
+++ New_DB_Project(GIS)/src/COM/Start/Main.java
@@ -0,0 +1,15 @@
+package com.Start;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	 public static ApplicationContext ctx;
+
+	    public static void main( String[] args ) {
+	        ctx = new ClassPathXmlApplicationContext( "bean/Start.xml" );
+	    }
+	}
+
+
